5eedacd3d70e70f7e84823a59687a8ac874c67f7
H368-cloud <hritikd369@gmail.com>
2024-12-17 01:51:30 +0530

    Add files via upload

---
+++ practice/script.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById('toggleMode');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        toggleButton.textContent = 'Switch to Light Mode';
+    } else {
+        toggleButton.textContent = 'Switch to Dark Mode';
+    }
+});
